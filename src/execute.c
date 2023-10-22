7ce7b1465e649615ac5549fc4a59b516592120f2
laulelien <aurelien.dlry@gmail.com>
2023-10-22 17:15:18 +0200

    qu 3

--- src/execute.c
+++ src/execute.c
@@ -1,14 +1,40 @@
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <wait.h>
+#include <assert.h>
 
 #include "readcmd.h"
 
+#define MAX_PROCESS_NUMBER 100
+
+pid_t pid_list[MAX_PROCESS_NUMBER];
+
 void execute(struct cmdline *l)
 {
-    char **cmd = l->seq[0];
-    pid_t pid = fork();
-    if (!pid)
+    int pid_nb = 0;
+    for (int i = 0; l->seq[i] != 0; i++)
     {
-        execvp(cmd[0], cmd);
+        char **cmd = l->seq[i];
+        pid_t pid = fork();
+        if (!pid)
+        {
+            execvp(cmd[0], cmd);
+        }
+        else
+        {
+            pid_list[pid_nb] = pid;
+        }
+        pid_nb++;
+    }
+
+    // We wait for all child processes to finish
+    if (!l->bg)
+    {
+        while (pid_nb)
+        {
+            pid_nb--;
+            waitpid(pid_list[pid_nb], NULL, 0);
+        }
     }
 }
